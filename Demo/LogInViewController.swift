e8ac1d8f7b67bafb26b8f4c9a8980ae5984050db
UKA19960324 <uka032486@gmail.com>
2017-07-12 22:08:04 +0800

    新增LogInViewController,與登入視圖控制器做關聯

---
+++ Demo/LogInViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LogInViewController.swift
+//  Demo
+//
+//  Created by U.K.A on 2017/7/12.
+//  Copyright © 2017年 U.K.A. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
