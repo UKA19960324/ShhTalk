992c07df48425c953fd8717b24631c8776f88cc5
UKA19960324 <uka032486@gmail.com>
2017-07-12 21:29:24 +0800

    Initial Commit

---
+++ Demo/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by U.K.A on 2017/7/11.
+//  Copyright © 2017年 U.K.A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginContainerView: UIView!
+    @IBOutlet weak var signUpContainerView: UIView!
+    
+    @IBAction func loginAction() {
+        loginContainerView.isHidden = false
+        signUpContainerView.isHidden = true
+    }
+    @IBAction func signUpAction() {
+        loginContainerView.isHidden = true
+        signUpContainerView.isHidden = false
+    }
+    //點選空白區域(鍵盤收合)
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        self.view.endEditing(true)
+    }
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//    }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+
+}
+
