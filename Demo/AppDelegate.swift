9489de86a87f276e4dc5cc606aa5394ad415d9ea
UKA19960324 <uka032486@gmail.com>
2017-07-25 12:22:30 +0800

    加入Firebase，完成註冊及驗證信部分。

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.\
         UIApplication.shared.statusBarStyle = .lightContent // 將狀態列更新為亮色系
+        FirebaseApp.configure()
         return true
     }
 
--- Demo/SignUpViewController.swift
+++ Demo/SignUpViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 class SignUpViewController: UIViewController {
     
     @IBOutlet weak var nameTextField: UITextField!
@@ -40,6 +40,46 @@ class SignUpViewController: UIViewController {
         }
     }
     
+    @IBAction func registerAccount(_ sender: UIButton) {
+        //輸入驗證
+        guard let name = nameTextField.text , name != "" ,
+                   let emailAddress = emailTextField.text , emailAddress != "" ,
+                   let password = passwordTextField.text , password != ""
+        else {
+            let alertController = UIAlertController(title: "Registration Error", message: "Please make sure you provide your name,email address and password to complete the registration",preferredStyle: .alert )
+            let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(okayAction)
+            present(alertController,animated: true,completion: nil)
+            return
+        }
+        //在Firebase 註冊使用者帳號
+        Auth.auth().createUser(withEmail: emailAddress , password: password , completion: {(user , error ) in
+            if let error = error {
+                let alertController = UIAlertController(title : "Registration Error" , message : error.localizedDescription,preferredStyle: .alert )
+                let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(okayAction)
+                self.present(alertController,animated: true,completion: nil)
+                return
+            }
+            //儲存使用者名稱
+            if let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest(){
+                changeRequest.displayName = name
+                changeRequest.commitChanges(completion: {(error: Error?) in
+                    if let error = error{
+                        print("Failed to change the display name: \(error.localizedDescription)")
+                    }
+                })
+            }
+            //移除鍵盤
+            self.view.endEditing(true)
+            //傳送驗證信
+            user?.sendEmailVerification(completion: nil)
+            let alertController = UIAlertController(title: "Email Verification", message: "We've just sent a confirmation email to your email address. Please check your inbox and click the verification link in that email to complete the sign up.",preferredStyle: .alert)
+            let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(okayAction)
+            self.present(alertController, animated: true, completion: nil)
+        })
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
