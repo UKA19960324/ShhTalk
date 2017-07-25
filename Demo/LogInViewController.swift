eea2cb0386700fd0c49026a3e2183f9cac043fc9
UKA19960324 <uka032486@gmail.com>
2017-07-25 15:34:47 +0800

    完成登入及判斷Email是否驗證。

--- Demo/LogInViewController.swift
+++ Demo/LogInViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 class LogInViewController: UIViewController {
 
     
@@ -51,7 +51,50 @@ class LogInViewController: UIViewController {
         nextButton.isHidden = true
         
     }
+    
+    @IBAction func loginAction(_ sender: UIButton) {
+        //輸入驗證
+        guard let emailAddress = emailTextField.text , emailAddress != "" ,
+                   let password = passwordTextField.text , password != ""
+        else {
+            let alertController = UIAlertController(title: "Login Error" , message: "Both fields must not be blank.", preferredStyle: .alert)
+            let okayAction = UIAlertAction(title: "OK" , style: .cancel,handler: nil)
+            alertController.addAction(okayAction)
+            present(alertController,animated: true, completion: nil)
+            return
+        }
+        //呼叫Firebase API 執行登入
+        Auth.auth().signIn(withEmail: emailAddress, password: password, completion: { (user , error) in
+            if let error = error{
+                let alertController = UIAlertController(title: "Login Error",message: error.localizedDescription,preferredStyle : .alert )
+                let okayAction = UIAlertAction(title: "OK", style: .cancel,handler: nil)
+                alertController.addAction(okayAction)
+                self.present(alertController, animated: true, completion: nil)
+                return
+            }
+            //Email驗證
+            guard let currentUser = user, currentUser.isEmailVerified
+                else{
+                    let alertController = UIAlertController(title: "Login Error",message: "You haven't confirmed your email address yet. We sent you a confirmation email when you sign up. Please click the verifiction link in that email. If you need us to send the confirmation email again, please tap Resend Email.",preferredStyle:
+                        .alert)
+                    let okayAction = UIAlertAction(title: "Resend email", style: .default,handler:{(action) in user?.sendEmailVerification(completion: nil)})
+                    let cancelAction = UIAlertAction(title: "Cancel",style: .cancel,handler: nil)
+                    alertController.addAction(okayAction)
+                    alertController.addAction(cancelAction)
+                    self.present(alertController, animated: true, completion: nil)
+                    return
+            }
 
+            //解除鍵盤
+            self.view.endEditing(true)
+            
+            //呈現主視圖
+            if let viewController = self.storyboard?.instantiateViewController(withIdentifier: "MyProfile"){
+                self.present(viewController, animated: true, completion: nil)
+            }
+        })
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
