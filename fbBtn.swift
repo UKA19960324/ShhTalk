9bdb608ed76bfcbbba6de5ff335d31d934f27689
王曉琪 <chi@wangxiaoqide-macbook-pro.local>
2017-08-13 15:58:26 +0800

    新增一fbBtn類別，用於設計按鈕外型。

---
+++ fbBtn.swift
@@ -0,0 +1,21 @@
+//
+//  fbBtn.swift
+//  
+//
+//  Created by 王曉琪 on 2017/8/13.
+//
+//
+
+import UIKit
+
+class fbBtn: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
