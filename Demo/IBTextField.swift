7cea22f3e2ed4debe5cb92f3b125cd2a0697aa2a
Chi <chi@wangxiaoqide-macbook-pro.local>
2017-09-07 15:38:48 +0800

    Create 'IBTextField' class.

---
+++ Demo/IBTextField.swift
@@ -0,0 +1,21 @@
+//
+//  IBTextField.swift
+//  Demo
+//
+//  Created by Chi on 2017/9/7.
+//  Copyright © 2017年 U.K.A. All rights reserved.
+//
+
+import UIKit
+
+class IBTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
