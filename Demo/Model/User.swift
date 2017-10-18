dc30c59d4dff6b1b28779e80713f9296c210c458
UKA19960324 <uka032486@gmail.com>
2017-10-19 00:12:07 +0800

    User類別裡新增讀取使用者好友資料的Method。

--- Demo/Model/User.swift
+++ Demo/Model/User.swift
@@ -47,4 +47,38 @@ class User : NSObject {
             }
         })
     }
+    
+    // 讀取使用者好友資料
+    class func downloadAllFriedns(forUserID: String, completion: @escaping (User) -> Void ){
+        Database.database().reference().child("users").child(forUserID).child("friends").observe(.childAdded, with: {(snapshot) in
+            //            print("======================")
+            //            print(snapshot)
+            //            print(snapshot.key)
+            //            print(snapshot.value )
+            //            print("======================")
+            let friendId = snapshot.key
+            Database.database().reference().child("users").child(friendId).child("credentials").observeSingleEvent(of: .value, with: { (snapshot) in
+                //                print("======================")
+                //                print(snapshot.value)          // 中文亂碼
+                //                print("======================")
+                if let data = snapshot.value as? [String:String]{
+                    let name = data["name"]!
+                    let email = data["email"]!
+                    //                    print("======================")
+                    //                    print(name)                    //顯示正常
+                    //                    print(email)
+                    //                    print(link)
+                    //                    print("======================")
+                    let link = URL.init(string: data["profilePicLink"]!)
+                    URLSession.shared.dataTask(with: link! , completionHandler: { (data, response , error) in
+                        if error == nil {
+                            let profilePic = UIImage.init(data: data!)
+                            let user = User.init(name: name, email: email, id: forUserID, profilePic: profilePic!)
+                            completion(user)
+                        }
+                    }).resume()
+                }
+            })
+        })
+    }
 }
