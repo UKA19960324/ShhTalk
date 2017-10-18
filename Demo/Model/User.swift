b944aeec5ab86fb5a8eb979b7d1ab03565345f24
UKA19960324 <uka032486@gmail.com>
2017-10-18 23:45:29 +0800

    建立User類別，負責以後連結資料庫使用者部分。

---
+++ Demo/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Demo
+//
+//  Created by U.K.A on 2017/10/18.
+//  Copyright © 2017年 U.K.A. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+class User : NSObject {
+    
+}
