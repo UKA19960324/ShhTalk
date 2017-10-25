255c49e7486edaeee1eda82a4331e712f4949485
UKA19960324 <uka032486@gmail.com>
2017-10-25 15:52:26 +0800

    建立Conversation類別，負責以後連結資料庫對話記錄部分。

---
+++ Demo/Model/Conversation.swift
@@ -0,0 +1,15 @@
+//
+//  Conversation.swift
+//  Demo
+//
+//  Created by U.K.A on 2017/10/25.
+//  Copyright © 2017年 U.K.A. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+class Conversation {
+    
+    
+}
