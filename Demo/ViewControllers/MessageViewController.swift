ce6f029d9e787a3d0c9205129c35666684dd3270
UKA19960324 <uka032486@gmail.com>
2017-10-25 17:03:19 +0800

    訊息列表頭貼載入完成。

--- Demo/ViewControllers/MessageViewController.swift
+++ Demo/ViewControllers/MessageViewController.swift
@@ -11,18 +11,33 @@ import Firebase
 class MessageViewController: UIViewController, UITableViewDataSource , UITableViewDelegate {
     
     @IBOutlet weak var messageTableView: UITableView!
-    var items = [User]()
+    var items = [Conversation]()
+    var selectedUser: User?
+    
     // Do any additional setup after loading the view.
     override func viewDidLoad() {
         super.viewDidLoad()
         addSideButton()
-        fetchUsers()
+        self.fetchData()
     }
  
     // Dispose of any resources that can be recreated.
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    // Downloads conversations
+    func fetchData() {
+        if let id = Auth.auth().currentUser?.uid {
+            Conversation.showConversations(forUserID: id , completion: { (conversations) in
+                DispatchQueue.main.async {
+                    self.items = conversations
+                    self.messageTableView.reloadData()
+                }
+            })
+        }
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
@@ -30,8 +45,8 @@ class MessageViewController: UIViewController, UITableViewDataSource , UITableVi
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellIdentifier = "Cell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! MessageTableViewCell
-        cell.nameLabel.text = items[indexPath.row].name
-        cell.photoImageView.image = items[indexPath.row].profilePic
+        cell.nameLabel.text = items[indexPath.row].user.name
+        cell.photoImageView.image = items[indexPath.row].user.profilePic
         return cell
     }
     //Downloads user friends list
@@ -45,14 +60,12 @@ class MessageViewController: UIViewController, UITableViewDataSource , UITableVi
             })
         }
     }
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
-
+    
 }
