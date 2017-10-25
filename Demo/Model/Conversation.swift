198b45fb03312c7744d72ac4b9514b0ec1a96438
UKA19960324 <uka032486@gmail.com>
2017-10-25 16:25:51 +0800

    User類別裡新增讀取對話記錄的Method。

--- Demo/Model/Conversation.swift
+++ Demo/Model/Conversation.swift
@@ -17,10 +17,28 @@ class Conversation {
 //    var lastMessage: Message
     
     //MARK: Inits
+    
     init(user: User/*, lastMessage: Message*/) {
         self.user = user
 //        self.lastMessage = lastMessage
     }
     
+    //MARK: Methods
+    class func showConversations(forUserID: String , completion: @escaping ([Conversation]) -> Swift.Void){
+        var conversations = [Conversation]()
+        Database.database().reference().child("users").child(forUserID).child("friends").observe(.childAdded, with: { (snapshot) in
+            let friendsId = snapshot.key
+            User.info(forUserID: friendsId , completion: { (user) in
+                Database.database().reference().child("users").child(user.id).child("conversations").observe(.childAdded, with: { (snapshot) in
+                    if snapshot.exists() {
+                    // ...
+                    }
+                })
+                let conversation = Conversation.init(user: user/*, lastMessage: emptyMessage*/)
+                conversations.append(conversation)
+                completion(conversations)
+            })
+        })
+    }
     
 }
