b1b246cf5bc3612a6ea4df5a028d6d27adfba4bc
UKA19960324 <uka032486@gmail.com>
2017-10-26 22:20:07 +0800

    Message類別裡新增屬性和初始化。

--- Demo/Model/Message.swift
+++ Demo/Model/Message.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import UIKit
+import Firebase
+class Message {
+
+    //MARK: Properties
+    var owner: MessageOwner
+    var type: MessageType
+    var content: Any
+    var timestamp: Int
+    var isRead: Bool
+    var image: UIImage?
+    private var toID: String?
+    private var fromID: String?
+    
+    //MARK: Inits
+    init(type: MessageType, content: Any, owner: MessageOwner, timestamp: Int, isRead: Bool) {
+        self.type = type
+        self.content = content
+        self.owner = owner
+        self.timestamp = timestamp
+        self.isRead = isRead
+    }
+    
+}
+
