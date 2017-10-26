85807038a395cd14c73f56728f08cbf01087ba85
UKA19960324 <uka032486@gmail.com>
2017-10-26 22:39:50 +0800

    Message類別裡新增傳送訊息的Method。

--- Demo/Model/Message.swift
+++ Demo/Model/Message.swift
@@ -12,6 +12,7 @@ import Firebase
 class Message {
 
     //MARK: Properties
+    
     var owner: MessageOwner
     var type: MessageType
     var content: Any
@@ -22,12 +23,44 @@ class Message {
     private var fromID: String?
     
     //MARK: Inits
+    
     init(type: MessageType, content: Any, owner: MessageOwner, timestamp: Int, isRead: Bool) {
         self.type = type
         self.content = content
         self.owner = owner
         self.timestamp = timestamp
         self.isRead = isRead
+    }
+    
+    //MARK: Methods
+    
+    class func send(message: Message, toID: String, completion: @escaping (Bool) -> Swift.Void)  {
+        if let currentUserID = Auth.auth().currentUser?.uid {
+//            switch message.type {
+//            case .location:
+//                let values = ["type": "location", "content": message.content, "fromID": currentUserID, "toID": toID, "timestamp": message.timestamp, "isRead": false]
+//                Message.uploadMessage(withValues: values, toID: toID, completion: { (status) in
+//                    completion(status)
+//                })
+//            case .photo:
+//                let imageData = UIImageJPEGRepresentation((message.content as! UIImage), 0.5)
+//                let child = UUID().uuidString
+//                FIRStorage.storage().reference().child("messagePics").child(child).put(imageData!, metadata: nil, completion: { (metadata, error) in
+//                    if error == nil {
+//                        let path = metadata?.downloadURL()?.absoluteString
+//                        let values = ["type": "photo", "content": path!, "fromID": currentUserID, "toID": toID, "timestamp": message.timestamp, "isRead": false] as [String : Any]
+//                        Message.uploadMessage(withValues: values, toID: toID, completion: { (status) in
+//                            completion(status)
+//                        })
+//                    }
+//                })
+//            case .text:
+            let values = ["type": "text", "content": message.content, "fromID": currentUserID, "toID": toID, "timestamp": message.timestamp, "isRead": false]
+            Message.uploadMessage(withValues: values, toID: toID, completion: { (status) in
+                completion(status)
+            })
+            //            }
+        }
     }
     
 }
