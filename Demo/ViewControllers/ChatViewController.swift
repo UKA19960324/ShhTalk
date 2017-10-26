3f059591d12d8573721d3fd95adf2a0612d5a7b9
UKA19960324 <uka032486@gmail.com>
2017-10-27 00:06:03 +0800

    在ChatViewController裡新增composeMessage函數，來傳送不同類型的訊息。

--- Demo/ViewControllers/ChatViewController.swift
+++ Demo/ViewControllers/ChatViewController.swift
@@ -22,11 +22,18 @@ class ChatViewController: UIViewController {
         print(currentUser?.name)
         print(currentUser?.id)
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        self.composeMessage(type: .text, content: " 測試3 23:58 ! " )
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func composeMessage(type: MessageType, content: Any)  {
+        let message = Message.init(type: type, content: content, owner: .sender, timestamp: Int(Date().timeIntervalSince1970), isRead: false)
+        Message.send(message: message, toID: self.currentUser!.id, completion: {(_) in
+        })
     }
     
 
