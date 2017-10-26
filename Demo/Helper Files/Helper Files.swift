4564562175f82887afbbc662138cab4f870eb29b
UKA19960324 <uka032486@gmail.com>
2017-10-26 22:07:52 +0800

    新增 Helper Files 類別 。

---
+++ Demo/Helper Files/Helper Files.swift
@@ -0,0 +1,22 @@
+//
+//  Helper Files.swift
+//  Demo
+//
+//  Created by U.K.A on 2017/10/26.
+//  Copyright © 2017年 U.K.A. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//Enums
+enum MessageType {
+    case photo
+    case text
+    case location
+}
+
+enum MessageOwner {
+    case sender
+    case receiver
+}
